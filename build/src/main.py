39964653199c06f0262d03e6872cd51b48a5ed98
clement-massit <clement.massit@gmail.com>
2024-09-19 15:01:01 +0200

    manage cors error

--- build/src/main.py
+++ build/src/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
 from src.routers import recipe
+
 
 
 
@@ -9,6 +11,31 @@ from src.routers import recipe
 app = FastAPI()
 
 app.include_router(recipe.router)
+
+origins = [
+    "http://localhost/",
+    "http://localhost:5000/",
+    "http://localhost:5173/",
+    "https://test.datapulp.net/api/",
+    "https://test.datapulp.net/",
+    "https://localhost/",
+    "https://localhost:5000/",   
+    "http://localhost:5174/",
+    "https://localhost:5174/",
+    "https://localhost:5173/",
+
+
+
+    ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 @app.get("/")
